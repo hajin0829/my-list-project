3abd16c55791363621071ccff869bac665220c75
chj <qwer970901@naver.com>
2026-01-29 22:53:04 +0900

    switch db from sqlite to MySQL

--- review_project/review_project/settings.py
+++ review_project/review_project/settings.py
@@ -75,10 +75,18 @@ WSGI_APPLICATION = 'review_project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'my_list_project',
+        'USER': 'hajin',
+        'PASSWORD': '060829',
+        'HOST': 'localhost',
+        'PORT': '3306',
+        'OPTIONS': {
+            'charset': 'utf8mb4',
+        },
     }
 }
+
 
 
 # Password validation
